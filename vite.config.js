2ac3346646f67b2c2c52457ed0d974619e03b1cd
albdar <darimont@darimont.de>
2026-08-26 12:31:43 +0200

    Fix wallet asset paths in vercel json, removed wrong vercile json

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,8 @@ import { defineConfig } from "vite";
 import { nodePolyfills } from "vite-plugin-node-polyfills";
 
 export default defineConfig({
+  base: "/wallets/",
+
   plugins: [
     nodePolyfills(),
   ],
